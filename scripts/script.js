d71a85b4a23dfede265e801fd41adbb91641b681
curtis.cholon@gmail.com <u80lbYFay7Su>
2019-07-05 11:12:05 -0700

    Updated styling

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,22 @@ function addEventListeners() {
     var cards = document.getElementsByClassName('card');
     for (let i=0; i<cards.length; i++) {
         cards[i].addEventListener('click', function(e){
-           description = this.getElementsByClassName('description')[0];
-           console.log(description.innerHTML);
-        })
+           description = this.getElementsByClassName('description')[0].innerHTML;
+           notes = this.getElementsByClassName('notes')[0].innerHTML;
+           roi = this.getElementsByClassName('roi')[0].innerHTML;
+
+           document.getElementById('description-content').innerHTML = description;
+           document.getElementById('roi-content').innerHTML = roi;
+           document.getElementById('notes-content').innerHTML = notes;
+           document.getElementById('overlay-wrapper').style.display = 'block';
+        });
     }
-};
+}
+
+
+function closeOverlay() {
+    document.getElementById('overlay-wrapper').style.display = 'none';
+}
+
 
 addEventListeners();
