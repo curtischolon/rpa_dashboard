fe8c19d785dbee55e39aa98cf181dde2cad5a7b7
unknown <curtis.cholon@gmail.com>
2019-07-06 08:10:26 -0700

    updating / changing styles

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,7 @@ function addEventListeners() {
     var cards = document.getElementsByClassName('card');
     for (let i=0; i<cards.length; i++) {
         cards[i].addEventListener('click', function(e){
-           description = this.getElementsByClassName('description')[0].innerHTML;
-           notes = this.getElementsByClassName('notes')[0].innerHTML;
-           roi = this.getElementsByClassName('roi')[0].innerHTML;
-
-           document.getElementById('description-content').innerHTML = description;
-           document.getElementById('roi-content').innerHTML = roi;
-           document.getElementById('notes-content').innerHTML = notes;
-           document.getElementById('overlay-wrapper').style.display = 'block';
+           document.getElementById('dropdown-content').style.display = 'block';
         });
     }
 }
