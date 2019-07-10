6e59376c0e8b39dd9ed956bfcc0507a43ddb65f8
curtis.cholon@gmail.com <u80lbYFay7Su>
2019-07-10 16:33:50 -0700

    updating php

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php include('server.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RPA Dashboard</title>
+</head>
+<body>
+    <form action="server.php" method="post">
+        <label for="dept">Department:</label>
+        <input type="text" name="dept" id="dept" require>
+        <label for="task_name">Task Name</label>
+        <input type="text" name="task_name" id="task_name" require>
+        <label for="perct_cmplt">Percentage Complete</label>
+        <input type="number" name="perct_cmplt" id="perct_cmplt" min="0" max="100" value="0">
+        <label for="priority">Priority</label>
+        <select name="priority" id="priority">
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+        </select>
+        <label for="description"></label>
+        <input type="text" name="description" id="description" require>
+        <label for="roi">ROI (hrs/week)</label>
+        <input type="number" name="roi" id="roi" require>
+        <label for="implemented">Date Implemented</label>
+        <input type="date" name="implemented" id="implemented">
+        <label for="completion">Target Completion Date</label>
+        <input type="date" name="completion" id="completion">
+        <button type="submit" name="save">Save</button>
+    </form>
+</body>
+</html>
---
+++ server.php
@@ -0,0 +1,48 @@
+<?php
+    session_start();
+    $db = mysqli_connect('10.0.245.18',
+    'admin', 
+    'QpT1dJyCBkXutIzX',
+    'db_rpa');
+
+    $dept = '';
+    $task_name = '';
+    $perct_complt = 0;
+    $priority = '';
+    $description = '';
+    $roi = 0;
+    $implemented = '';
+    $completion = '';
+    $update = false;
+
+    if (isset($_POST['save'])) {
+        $dept = $_POST['dept'];
+        $task_name = $_POST['task_name'];
+        $perct_cmplt = $_POST['perct_cmplt'];
+        $priority = $_POST['priority'];
+        $description = $_POST['description'];
+        $roi = $_POST['roi'];
+        $implemented = $_POST['implemented'];
+        $completion = $_POST['completion'];
+
+        mysqli_query($db, "INSERT INTO tbl_rpa (dept, 
+        task_name, 
+        percentage_complete, 
+        priority, 
+        description,
+        roi,
+        implemented,
+        completion) 
+        VALUES
+        ('$dept', 
+        '$task_name', 
+        '$perct_cmplt',
+        '$priority',
+        '$description',
+        '$roi',
+        '$implemented',
+        '$completion')");
+        $_SESSION['message'] = "Item Saved";
+        header('location: index.php');
+    }
+?>
