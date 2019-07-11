c09308be1b4f115f853e119e0bf109e069a273a8
curtis.cholon@gmail.com <u80lbYFay7Su>
2019-07-11 15:43:15 -0700

    updating content

--- index.php
+++ index.php
@@ -6,29 +6,72 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>RPA Dashboard</title>
+    <link rel="stylesheet" href="/rpa/stylesheets/formStyle.css">
+    <link href="https://fonts.googleapis.com/css?family=Covered+By+Your+Grace|Mitr:300,500,600|Roboto:400,700&display=swap" rel="stylesheet">
 </head>
 <body>
+    <header>
+        Task Automation Submission Form
+    </header>
     <form action="server.php" method="post">
-        <label for="dept">Department:</label>
-        <input type="text" name="dept" id="dept" require>
-        <label for="task_name">Task Name</label>
-        <input type="text" name="task_name" id="task_name" require>
-        <label for="perct_cmplt">Percentage Complete</label>
-        <input type="number" name="perct_cmplt" id="perct_cmplt" min="0" max="100" value="0">
-        <label for="priority">Priority</label>
-        <select name="priority" id="priority">
-            <option value="low">Low</option>
-            <option value="medium">Medium</option>
-            <option value="high">High</option>
-        </select>
-        <label for="description">Description</label>
-        <input type="text" name="description" id="description" require>
-        <label for="roi">ROI (hrs/week)</label>
-        <input type="number" name="roi" id="roi" require>
-        <label for="implemented">Date Implemented</label>
-        <input type="date" name="implemented" id="implemented">
-        <label for="completion">Target Completion Date</label>
-        <input type="date" name="completion" id="completion">
+        <div class="col-25">
+            <label for="dept">Department:</label>
+        </div>
+        <div class="col-75">
+            <input type="text" name="dept" id="dept" require>
+        </div>
+        <div class="col-25">
+            <label for="task_name">Task Name</label>
+        </div>
+        <div class="col-75">
+            <input type="text" name="task_name" id="task_name" require>
+        </div>
+        <div class="col-25">
+            <label for="perct_cmplt">Percentage Complete</label>
+        </div>
+        <div class="col-75">
+            <input type="number" name="perct_cmplt" id="perct_cmplt" min="0" max="100" value="0">
+        </div>
+        <div class="col-25">
+            <label for="priority">Priority</label>
+        </div>
+        <div class="col-75">
+            <select name="priority" id="priority">
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+            </select>
+        </div>
+        <div class="col-25">
+            <label for="description">Description</label>
+        </div>
+        <div class="col-75">
+            <textarea type="text" name="description" id="description" require></textarea>
+        </div>
+        <div class="col-25">
+            <label for="roi">ROI (hrs/week)</label>
+        </div>
+        <div class="col-75">
+            <input type="number" name="roi" id="roi" require>
+        </div>
+        <div class="col-25">
+            <label for="implemented">Date Implemented</label>
+        </div>
+        <div class="col-75">
+            <input type="date" name="implemented" id="implemented">
+        </div>
+        <div class="col-25">
+            <label for="completion">Target Completion Date</label>
+        </div>
+        <div class="col-75">
+            <input type="date" name="completion" id="completion">
+        </div>
+        <div class="col-25">
+            <label for="notes">Notes:</label>
+        </div>
+        <div class="col-75">
+            <textarea name="notes" id="notes" cols="30" rows="10"></textarea>
+        </div>
         <button type="submit" name="save">Save</button>
     </form>
 </body>
--- server.php
+++ server.php
@@ -2,7 +2,7 @@
     session_start();
     $db = mysqli_connect('10.0.245.18',
     'admin', 
-    '',
+    'password',
     'db_rpa');
 
     $dept = '';
@@ -13,6 +13,7 @@
     $roi = 0;
     $implemented = '';
     $completion = '';
+    $notes = '';
     $update = false;
 
     if (isset($_POST['save'])) {
@@ -24,15 +25,17 @@
         $roi = $_POST['roi'];
         $implemented = $_POST['implemented'];
         $completion = $_POST['completion'];
+        $notes = $_POST['notes'];
 
         mysqli_query($db, "INSERT INTO tbl_rpa (dept, 
         task_name, 
         percentage_complete, 
-        priority, 
+        priority,
         description,
         roi,
         implemented,
-        completion) 
+        completion,
+        notes) 
         VALUES
         ('$dept', 
         '$task_name', 
@@ -41,7 +44,8 @@
         '$description',
         '$roi',
         '$implemented',
-        '$completion')");
+        '$completion',
+        '$notes')");
         $_SESSION['message'] = "Item Saved";
         header('location: index.php');
     }
